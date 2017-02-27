84d9589929de54ed2f1e2ea906d5404294f51502
tkemaladze <tkemaladze.k.a@gmail.com>
2017-02-27 20:12:14 +0400

    сделал коммит 5

---
+++ src/ru/levelp/Bla.java
@@ -0,0 +1,7 @@
+package ru.levelp;
+
+/**
+ * Created by Student06 on 27.02.2017.
+ */
+public interface Bla {
+}
---
+++ src/ru/levelp/HelloWorld.java
@@ -0,0 +1,13 @@
+package ru.levelp;
+
+import javax.swing.*;
+
+/**
+ * Created by Student06 on 27.02.2017.
+ */
+public class HelloWorld extends JFrame implements Bla {
+    public static void main(String[] args) {
+        JButton button = new JButton();
+
+    }
+}
